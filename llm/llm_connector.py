ed7a9c6b4cc7f3697b0f634edb3ddf20c32984e6
Yash Revannavar <106887362+YashRevannavar@users.noreply.github.com>
2024-08-04 11:03:44 +0200

    llm setup done

---
+++ llm/llm_connector.py
@@ -0,0 +1,11 @@
+from langchain_mistralai import ChatMistralAI
+from extras.constants import MISTRAL_MODEL
+
+
+def get_mistral_model():
+    llm = ChatMistralAI(
+        model=MISTRAL_MODEL,
+        temperature=0,
+        max_retries=2
+    )
+    return llm
